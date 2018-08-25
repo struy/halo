4f305dfb019e238eab5a777aff7bdcbe21fc4088
struy <dubovykwork@gmail.com>
2018-08-24 22:08:07 -0500

    add template home page

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
