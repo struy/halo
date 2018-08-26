d67956ca50a3cc71cb95c80295b0338ba3f74a37
struy <dubovykwork@gmail.com>
2018-08-25 19:13:54 -0500

    previous templates

--- app.py
+++ app.py
@@ -1,25 +1,42 @@
 from flask import Flask
-from flask import flash, redirect, render_template, request, session, abort
+from flask import flash, redirect, render_template, request, session, abort, url_for
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_sqlalchemy import SQLAlchemy
+from models import User, Entity
+
 import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index():
-    if not session.get('logged_in'):
-        return render_template('login.html')
-    else:
-        return render_template('index.html')
+    return render_template('index.html')
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def do_admin_login():
-    if request.form['password'] == 'password' and request.form['username'] == 'admin':
-        session['logged_in'] = True
+    if request.method == 'POST':
+        if request.form['password'] == 'password' and request.form['username'] == 'admin':
+            session['logged_in'] = True
+        else:
+            flash('wrong password!')
+        return index()
     else:
-        flash('wrong password!')
-    return index()
+        return render_template('login.html')
+
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    """Register Form"""
+    if request.method == 'POST':
+        new_user = User(username=request.form['username'], password=request.form['password'])
+        db.session.add(new_user)
+        db.session.commit()
+        return render_template('login.html')
+    return render_template('register.html')
 
 
 @app.route("/logout")
@@ -32,13 +49,14 @@ def logout():
 def get():
     pass
 
+
 @app.route("/set")
-def get():
+def set():
     pass
 
 
-
 if __name__ == '__main__':
-    app.debug=True
+    app.debug = True
     app.secret_key = os.urandom(15)
+    toolbar = DebugToolbarExtension(app)
     app.run(port=2020)
